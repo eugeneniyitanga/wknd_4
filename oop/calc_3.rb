e7de396f5e661c21ef0d43b7b0256d6fb762fa5e
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-02-05 23:29:24 +0200

    used inheritance to DRY up the codes for three different classes

--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -4,43 +4,15 @@
 # DRY up all the code below - there shouldn't be a single method duplicated between
 # any two classes.
 
+require "./module.rb"
+
 class SimpleCalculator
 
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+include Calculators 
 
 end
 
-class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+class FancyCalculator <SimpleCalculator
 
   def square_root(number)
     Math.sqrt(number)
@@ -48,27 +20,7 @@ class FancyCalculator
 
 end
 
-class WhizBangCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
-  def square_root(number)
-    Math.sqrt(number)
-  end
+class WhizBangCalculator <FancyCalculator
 
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
@@ -84,3 +36,73 @@ end
 
 # Copy your driver code from the previous exercise and more below:
 
+simplecalculator = SimpleCalculator.new 
+fancycalculator = FancyCalculator.new 
+
+add = fancycalculator.add(10, 6)
+subtract = fancycalculator.subtract(24, 4)
+multiply = fancycalculator.multiply(2,4)
+divide = fancycalculator.divide(15, 3)
+square_root = fancycalculator.square_root(4)
+
+result = add 
+
+puts "fancycalculator returned:"
+puts result 
+
+if result == 16 
+  puts "Success"
+
+else 
+  "F"
+
+end 
+
+result = subtract 
+
+puts "fancycalculator returned:"
+puts result 
+
+if result == 20 
+  puts "Success"
+
+else 
+  "F"
+end 
+
+result = multiply 
+
+puts "fancycalculator returned:"
+puts result 
+
+if result == 8 
+  puts "Success"
+
+else 
+  "F"
+end 
+
+result = divide 
+
+puts "fancycalculator returned:"
+puts result 
+
+if result == 5 
+  puts "Success"
+
+else 
+  "F"
+end 
+
+result = square_root
+
+
+puts "fancycalculator returned:"
+puts result 
+
+if result == 2
+puts "Success"
+
+else 
+  "F"
+end 
